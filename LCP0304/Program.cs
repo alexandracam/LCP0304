3ababe5e00829d1da2b24b358d1a8cc750cc548e
Alexandra Camarena <alexandracamarena@ou.edu>
2024-03-01 20:50:51 -0600

    Add project files.

---
+++ LCP0304/Program.cs
@@ -0,0 +1,26 @@
+﻿// MIS 3013 001
+// March 4, 2024
+//Alexandra Camarena
+// 113568154
+
+Console.WriteLine("Functions");
+
+// define a function, calculate the ratio
+// accept two params
+// return a percentage string
+// CalRatio
+
+string CalRatio(int n1, int n2)
+{
+    double r;
+    r = 1.0 * n1 / n2;
+    string pStr;
+    pStr = $"{r:P2}";
+    return pStr;
+}
+
+string rStr;
+
+rStr = CalRatio(5, 20);
+
+Console.WriteLine(rStr);
